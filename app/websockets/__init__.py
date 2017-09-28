76eeac8421d678ea9107f8f8096124aba8d4fcfe
Samuel Murillo <samuelmurillo333@gmail.com>
2017-09-28 01:05:09 -0400

    Rename blueprint

--- app/websockets/__init__.py
+++ app/websockets/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-main = Blueprint('main', __name__)
+websockets_blueprint = Blueprint('websockets', __name__)
 
-from . import events
+from app.websockets import events
